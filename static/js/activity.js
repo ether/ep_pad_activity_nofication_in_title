bd7c38870a1fda3d2297b5582ba30088dd619a12
John McLear <john@mclear.co.uk>
2020-11-27 19:55:06 +0000

    autofixes from Etherpad checkPlugins.js

--- static/js/activity.js
+++ static/js/activity.js
@@ -1,48 +1,48 @@
 var _ = require('ep_etherpad-lite/static/js/underscore');
 
-if(typeof exports == 'undefined'){
-  var exports = this['mymodule'] = {};
+if (typeof exports === 'undefined') {
+  var exports = this.mymodule = {};
 }
 
-exports.postAceInit = function(hook, context){
-  context.ace.callWithAce(function(ace){
-    var doc = ace.ace_getDocument();
+exports.postAceInit = function (hook, context) {
+  context.ace.callWithAce((ace) => {
+    const doc = ace.ace_getDocument();
     $(doc).find('#innerdocbody').mousemove(_(exports.userActive).bind(ace));
     $(doc).find('#innerdocbody').keypress(_(exports.userActive).bind(ace));
   }, 'hovering', true);
-}
+};
 
 
-exports.aceEditEvent = function(hook_name, args, cb){
+exports.aceEditEvent = function (hook_name, args, cb) {
   try {
     window.top.document;
-  } catch(e) {
+  } catch (e) {
     /* top-level document is not accessible */
     return;
   }
 
-  var caretMoving = ( args.callstack.type == "applyChangesToBase" );
-  if(!caretMoving) return false;
+  const caretMoving = (args.callstack.type == 'applyChangesToBase');
+  if (!caretMoving) return false;
 
-  if(window.top.document.title[0] !== "*"){
-    if(window.top.document.title[0] === "*"){
-      var prevTitle = window.top.document.title.substring(2,window.top.document.title.length);
-    }else{
+  if (window.top.document.title[0] !== '*') {
+    if (window.top.document.title[0] === '*') {
+      var prevTitle = window.top.document.title.substring(2, window.top.document.title.length);
+    } else {
       var prevTitle = window.top.document.title;
     }
-    var newTitle = "* "+prevTitle
+    const newTitle = `* ${prevTitle}`;
     window.top.document.title = newTitle;
   }
-}
+};
 
-exports.userActive = function(){
+exports.userActive = function () {
   try {
-    window.top.document; 
-  } catch(e) { 
-    /* top-level document is not accessible */ 
-    return; 
-  } 
-  if(window.top.document.title[0] == "*"){
-    window.top.document.title = window.top.document.title.substring(1,window.top.document.title.length);
+    window.top.document;
+  } catch (e) {
+    /* top-level document is not accessible */
+    return;
   }
-}
+  if (window.top.document.title[0] == '*') {
+    window.top.document.title = window.top.document.title.substring(1, window.top.document.title.length);
+  }
+};
